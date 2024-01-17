f28637a7967b27a3f1380b2dda3bc7efb08cf131
pikaz-18 <qq2363994579@outlook.com>
2024-01-18 00:09:20 +0800

    fixreadme

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
 /*
- * @Description: 这是***页面（组件）
+ * @Description: 这是配置页面（组件）
  * @Date: 2021-12-26 22:58:52
  * @Author: zouzheng
  * @LastEditors: zouzheng
- * @LastEditTime: 2023-01-05 20:30:28
+ * @LastEditTime: 2024-01-18 00:09:10
  */
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require("copy-webpack-plugin")
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     mode: 'production',
@@ -18,7 +18,7 @@ module.exports = {
         path: path.resolve(__dirname, 'lib'),
         libraryTarget: 'umd',
         library: 'pikazLocation',
-        globalObject: 'this'
+        globalObject: 'this',
     },
     plugins: [
         new CleanWebpackPlugin(),
@@ -26,9 +26,9 @@ module.exports = {
             patterns: [
                 {
                     from: path.resolve(__dirname, 'static'),
-                    to: path.resolve(__dirname, 'lib', 'static')
-                }
-            ]
-        })
-    ]
-};
+                    to: path.resolve(__dirname, 'lib', 'static'),
+                },
+            ],
+        }),
+    ],
+}
